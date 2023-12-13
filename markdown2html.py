aeda06de2111e8486485afe763ba947ec81609c7
VotreNom <shawanbarua70@gmail.com>
2023-12-13 15:24:11 +0100

    mark

--- markdown2html.py
+++ markdown2html.py
@@ -1,18 +1,2 @@
 #!/usr/bin/python3
 """ this is my function"""
-import sys
-import os.path
-
-path = "/" + sys.argv[1]
-
-check_file = os.path.isfile(path)
-
-if len(sys.argv) != 3:
-    print("Usage: ./markdown2html.py README.md README.html")
-    sys.exit(1)
-
-if sys.argv[1].split("."):
-    print("true")
-
-if check_file == False:
-    print("Missing README.md")
